489a9a858fcf4c53fbd1cdaa5511be619ae18b7a
Marcelo <infopontes@gmail.com>
2022-10-02 21:56:08 -0300

    Adicionei django_extensions

--- siof/settings/settings.py
+++ siof/settings/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # apps de terceiros
+    'django_extensions',
     # apps do projeto
     "users.apps.UsersConfig",
 ]
