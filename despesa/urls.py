b8d330b11a8e3b35add7a91a2eb1c6bc61dd08b5
Marcelo Pontes <infopontes@gmail.com>
2022-10-21 11:09:12 -0300

    Iniciar o desenvolvimento da interface para lançar os PTrab.

--- despesa/urls.py
+++ despesa/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
 from rest_framework import routers
 
+from despesa import views as v
+
 
 from despesa.api.viewsets import DespesaViewSet, DespesaAutenticationView
 
@@ -12,6 +14,7 @@ router.register(r'despesas', DespesaViewSet, basename='despesas')
 
 
 urlpatterns = [
+    path('ptrab/', v.index, name='index'),
     path('api/v1/', include(router.urls)),
     path('api/v1/despesa-autentication/', DespesaAutenticationView.as_view())
 ]
